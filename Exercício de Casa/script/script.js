377601ad5e1fb8cc5be4766b4476ef241f1a6a73
Mariana Herreros <maribranding@gmail.com>
2021-04-30 20:22:13 -0300

    Ajustes finais

--- Exercício de Casa/script/script.js
+++ Exercício de Casa/script/script.js
@@ -9,48 +9,49 @@ function exibirDados(event) {
     let dataInsercao = new Date().toLocaleDateString('pt-br');
     let horarioInsercao = new Date().toLocaleTimeString('pt-br');
 
-    if(pegaAutor === "") {
+    if (pegaAutor === "") {
         alert('Digite o nome do Autor.')
-    } else if(pegaTitulo === "") {
+    } else if (pegaTitulo === "") {
         alert('Digite o Título.')
-    }else if(pegaISBN === ""){
+    } else if (pegaISBN === "") {
         alert('Digite o ISBN.')
-    }else if(pegaDataPublicacao === ""){
+    } else if (pegaDataPublicacao === "") {
         alert('Digite a Data de Publicação.')
-    } else{
+    } else {
 
- 
-    document.getElementById('resposta').innerHTML += `
-        <ul>
-            <li>
-                <strong>Autor: </strong> ${pegaAutor}, <strong>Título: </strong> ${pegaTitulo},
-                <strong>ISBN: </strong> ${pegaISBN}, <strong>Data de Publicação</strong> ${pegaDataPublicacao},
-                <strong>Data de Ingresso no sistema</strong> ${dataInsercao}, ${horarioInsercao}
-                <a href="#" class="delete">Deletar Publicação</a>
-            </li>
-        </ul>
-`
-        
+
+        document.getElementById('resposta').innerHTML += `
+    <tr >
+        <td> ${pegaAutor} </td>
+        <td> ${pegaTitulo}</td>
+        <td> ${pegaDataPublicacao} </td>
+        <td>${pegaISBN}</td>
+        <td> ${dataInsercao}</td>
+        <td> ${horarioInsercao}</td>
+        <td> <button class="delete">Deletar</button></td>
+    </tr>
+    `
+
+
         limparDados();
     }
-    }
+}
 
 
 function limparDados() {
     document.getElementById('author').value = "";
     document.querySelector('#titulo').value = "";
     document.querySelector('#isbn').value = "";
-    document.getElementById('dataPublicacao').value = "";
+    document.getElementById('dataPublicacao').value = 
+    "";
 
 }
 
-document.getElementById('resposta').addEventListener('click', function(event) {
+document.getElementById('resposta').addEventListener('click', function (event) {
     if (event.target.className === "delete") {
-        event.target.parentElement.remove();
- 
+       let botao = event.target.parentElement;
+       botao.parentElement.remove();
+
     }
 })
-
-
- 
 
